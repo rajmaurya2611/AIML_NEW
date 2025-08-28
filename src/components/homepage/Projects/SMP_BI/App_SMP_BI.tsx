79aafcfc1943e2c37d8895172a2bb1545c8bea77
Maurya <Raj.Maurya@motherson.com>
2025-08-28 10:59:27 +0530

    Fixed errors

--- src/components/homepage/Projects/SMP_BI/App_SMP_BI.tsx
+++ src/components/homepage/Projects/SMP_BI/App_SMP_BI.tsx
@@ -2,27 +2,19 @@ import { Toaster } from "./ui_SMP_BI/toaster";
 import { Toaster as Sonner } from "./ui_SMP_BI/sonner";
 import { TooltipProvider } from "./ui_SMP_BI//tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom";
 import Index from "./pages_SMP_BI/Index";
-import NotFound from "./pages_SMP_BI/NotFound";
 import './index_SMP_BI.css'; 
 
 const queryClient = new QueryClient();
 
-const App = () => (
+const App_SMP_BI = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      
+      <Index/> 
     </TooltipProvider>
   </QueryClientProvider>
 );
 
-export default App;
+export default App_SMP_BI;
